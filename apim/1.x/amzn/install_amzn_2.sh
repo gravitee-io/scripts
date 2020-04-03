50bbb8c0bd2071831cf04a91fa353e52bcd81e1f
David BRASSELY <brasseld@gmail.com>
2020-04-03 08:52:38 +0200

    Wrong copy and paste...

--- apim/1.x/amzn/install_amzn_2.sh
+++ apim/1.x/amzn/install_amzn_2.sh
@@ -17,6 +17,19 @@ gpgkey=https://www.mongodb.org/static/pgp/server-3.6.asc" | sudo tee /etc/yum.re
     sudo systemctl start mongod
 }
 
+install_elasticsearch() {
+    echo "[elasticsearch-7.x]
+name=Elasticsearch repository for 7.x packages
+baseurl=https://artifacts.elastic.co/packages/7.x/yum
+gpgcheck=1
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+enabled=1
+autorefresh=1
+type=rpm-md" | sudo tee /etc/yum.repos.d/elasticsearch.repo > /dev/null
+    sudo yum install -y elasticsearch
+    sudo systemctl start elasticsearch
+}
+
 install_graviteeio() {
     echo "[graviteeio]
 name=graviteeio
@@ -28,13 +41,12 @@ sslverify=1
 sslcacert=/etc/pki/tls/certs/ca-bundle.crt
 metadata_expire=300" | sudo tee /etc/yum.repos.d/graviteeio.repo > /dev/null
     sudo yum -q makecache -y --disablerepo='*' --enablerepo='graviteeio'
-    sudo yum install -y graviteeio-am
+    sudo yum install -y graviteeio-api
     sudo systemctl daemon-reload
-    sudo systemctl start graviteeio-am-gateway graviteeio-am-management-api
-    sudo sed -i -e "s/4200/8084/g" /opt/graviteeio/am/management-ui/constants.json
+    sudo systemctl start graviteeio-apim-gateway graviteeio-apim-management-api
     http_response=$(curl -w "%{http_code}" -o /tmp/curl_body "http://169.254.169.254/latest/meta-data/public-ipv4")
     if [ $http_response == "200" ]; then
-        sudo sed -i -e "s/localhost/$(cat /tmp/curl_body)/g" /opt/graviteeio/am/management-ui/constants.json
+        sudo sed -i -e "s/localhost/$(cat /tmp/curl_body)/g" /opt/graviteeio/apim/management-ui/constants.json
     fi
 
     sudo systemctl restart nginx
@@ -48,6 +60,7 @@ main() {
     install_openjdk
     install_nginx
     install_mongo
+    install_elasticsearch
     install_graviteeio
 }
 
