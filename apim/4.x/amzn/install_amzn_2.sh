9182eec115eb47c852da6ba0bc42f2d02378e6b4
sebdevaux <sebastien.devaux@graviteesource.com>
2023-06-15 15:08:12 +0200

    feat(apim): add installation scripts for v4

---
+++ apim/4.x/amzn/install_amzn_2.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+install_nginx() {
+
+    echo "[nginx-stable]
+name=nginx stable repo
+baseurl=http://nginx.org/packages/amzn2/\$releasever/\$basearch/
+gpgcheck=1
+enabled=1
+gpgkey=https://nginx.org/keys/nginx_signing.key
+module_hotfixes=true
+priority=9" | sudo tee /etc/yum.repos.d/nginx.repo > /dev/null
+
+    sudo yum install -y nginx
+    sudo systemctl start nginx
+}
+
+install_mongo() {
+    case "`uname -i`" in
+      x86_64|amd64)
+        baseurl=https://repo.mongodb.org/yum/amazon/2/mongodb-org/6.0/x86_64/;;
+      aarch64)
+        baseurl=https://repo.mongodb.org/yum/amazon/2/mongodb-org/6.0/aarch64/;;
+    esac
+    echo "[mongodb-org-6.0]
+name=MongoDB Repository
+baseurl=${baseurl}
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-6.0.asc" | sudo tee /etc/yum.repos.d/mongodb-org-6.0.repo > /dev/null
+
+    sudo yum install -y mongodb-org
+    sudo systemctl start mongod
+}
+
+install_elasticsearch() {
+    echo "[elasticsearch]
+name=Elasticsearch repository for 8.x packages
+baseurl=https://artifacts.elastic.co/packages/8.x/yum
+gpgcheck=1
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+enabled=1
+autorefresh=1
+type=rpm-md" | sudo tee /etc/yum.repos.d/elasticsearch.repo > /dev/null
+    sudo yum install -y elasticsearch
+    sudo systemctl start elasticsearch
+}
+
+install_graviteeio() {
+    echo "[graviteeio]
+name=graviteeio
+baseurl=https://packagecloud.io/graviteeio/rpms/el/7/\$basearch
+gpgcheck=0
+enabled=1
+gpgkey=https://packagecloud.io/graviteeio/rpms/gpgkey
+sslverify=1
+sslcacert=/etc/pki/tls/certs/ca-bundle.crt
+metadata_expire=300" | sudo tee /etc/yum.repos.d/graviteeio.repo > /dev/null
+    sudo yum -q makecache -y --disablerepo='*' --enablerepo='graviteeio'
+    sudo yum install -y graviteeio-apim-4x
+    sudo systemctl daemon-reload
+    sudo systemctl start graviteeio-apim-gateway graviteeio-apim-rest-api
+    http_response=$(curl -w "%{http_code}" -o /tmp/curl_body "http://169.254.169.254/latest/meta-data/public-ipv4")
+    if [ $http_response == "200" ]; then
+        sudo sed -i -e "s/localhost/$(cat /tmp/curl_body)/g" /opt/graviteeio/apim/management-ui/constants.json
+        sudo sed -i -e "s/localhost/$(cat /tmp/curl_body)/g" /opt/graviteeio/apim/portal-ui/assets/config.json
+    fi
+
+    sudo systemctl restart nginx
+}
+
+install_openjdk() {
+    sudo yum install -y java-17-amazon-corretto-devel
+}
+
+main() {
+    install_openjdk
+    install_nginx
+    install_mongo
+    install_elasticsearch
+    install_graviteeio
+}
+
+main
---
+++ apim/4.x/sles/install_sles.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+install_nginx() {
+    sudo zypper addrepo -G -t yum -c 'http://nginx.org/packages/sles/15' nginx
+    sudo rpm --import http://nginx.org/keys/nginx_signing.key
+    sudo zypper -n install nginx
+}
+
+install_mongo() {
+    sudo rpm --import https://www.mongodb.org/static/pgp/server-6.0.asc
+    sudo zypper addrepo --gpgcheck "https://repo.mongodb.org/zypper/suse/15/mongodb-org/6.0/x86_64/" mongodb
+    sudo zypper -n install mongodb-org
+    sudo systemctl start mongod
+}
+
+install_elasticsearch() {
+    sudo rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
+    echo "[elasticsearch]
+name=Elasticsearch repository for 8.x packages
+baseurl=https://artifacts.elastic.co/packages/8.x/yum
+gpgcheck=1
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+enabled=1
+autorefresh=1
+type=rpm-md" | sudo tee /etc/zypp/repos.d/elasticsearch.repo > /dev/null
+    sudo zypper modifyrepo --enable elasticsearch
+    sudo zypper -n install elasticsearch
+    sudo zypper modifyrepo --disable elasticsearch
+    sudo systemctl start elasticsearch
+}
+
+install_graviteeio() {
+    echo "[graviteeio]
+name=graviteeio
+baseurl=https://packagecloud.io/graviteeio/rpms/el/7/\$basearch
+gpgcheck=0
+enabled=1
+gpgkey=https://packagecloud.io/graviteeio/rpms/gpgkey
+sslverify=1
+sslcacert=/etc/pki/tls/certs/ca-bundle.crt
+metadata_expire=300
+type=rpm-md" | sudo tee /etc/zypp/repos.d/graviteeio.repo > /dev/null
+    sudo zypper -n install graviteeio-apim-4x
+    sudo systemctl daemon-reload
+    sudo systemctl start graviteeio-apim-gateway graviteeio-apim-rest-api
+    http_response=$(curl -w "%{http_code}" -o /tmp/curl_body "http://169.254.169.254/latest/meta-data/public-ipv4")
+    if [ $http_response == "200" ]; then
+        sudo sed -i -e "s/localhost/$(cat /tmp/curl_body)/g" /opt/graviteeio/apim/management-ui/constants.json
+        sudo sed -i -e "s;/portal;http://$(cat /tmp/curl_body):8083/portal;g" /opt/graviteeio/apim/portal-ui/assets/config.json
+    fi
+    sudo systemctl restart nginx
+}
+
+install_openjdk() {
+  version=$(cut -d "=" -f2 <<< `cat /etc/os-release | grep VERSION_ID` | tr -d '"')
+  if [[ $version = 12* ]]; then
+    sudo zypper addrepo https://download.opensuse.org/repositories/Java:Factory/SLE_12_SP5/Java:Factory.repo
+    sudo zypper --gpg-auto-import-keys ref
+    sudo zypper refresh
+  fi
+
+  sudo zypper -n install java-17-openjdk
+}
+
+main() {
+    install_openjdk
+    install_nginx
+    install_mongo
+    install_elasticsearch
+    install_graviteeio
+}
+
+main
